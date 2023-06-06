87f9458588d045022c4eb782b21e279f68f17c41
Shreya-sen2520 <135489954+Shreya-sen2520@users.noreply.github.com>
2023-06-06 08:12:11 +0530

    Add files via upload

---
+++ mergetwosortedarray.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int n, int m) {
+	vector<int> merged;
+   for(int i=0; i<arr1.size();i++){
+        if(arr1[i]==0){
+            continue;
+        }
+       merged.push_back(arr1[i]);
+   }
+
+    for(int i=0; i<arr2.size();i++){
+        if(arr2[i]==0){
+            continue;
+        }
+       merged.push_back(arr2[i]);
+   }
+
+   sort(merged.begin(), merged.end());
+   return merged;
+
+ 
+}
