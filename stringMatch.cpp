84ac130737f5c3f7bf657da22ea42c1357809283
Shreya-sen2520 <135489954+Shreya-sen2520@users.noreply.github.com>
2023-07-29 12:04:47 +0530

    Add files via upload

--- stringMatch.cpp
+++ stringMatch.cpp
@@ -1,22 +1,34 @@
-#include <iostream>
-#include <vector>
-using namespace std;
 vector<int> stringMatch(string text, string pattern) {
-	vector<int> positions;  
-    int n = text.length();
-    int m = pattern.length();
-    for (int i = 0; i <= n - m; i++) {
-        bool match = true;
-	for (int j = 0; j < m; j++) {
-            if (text[i + j] != pattern[j]) {
-                match = false;
-                break;
-            }
-        }
-        if (match) {
-            positions.push_back(i + 1);  
-        }
 
-    }
-    return positions;
-}
+	int n = text.length(), m = pattern.length();
+	vector<int>ans, LPS(m, 0);
+	int len = 0;
+	for(int i = 1; i < m; i++){
+		if(pattern[i] == pattern[len]){
+			len++;
+			LPS[i] = len;
+		}
+		else{
+			if(len != 0){
+				len = LPS[len-1];
+			}
+		}
+	}
+
+	int i = 0, j = 0;
+
+	while(i < n){
+		if(text[i] == pattern[j]){
+			i++;
+			j++;
+			if(j == m){
+				ans.push_back(i-j+1);
+				j = LPS[j-1];
+			} 
+		}
+		else {
+			if(j > 0) j = LPS[j-1];
+			else i++;
+		}
+	}
+	return ans;
